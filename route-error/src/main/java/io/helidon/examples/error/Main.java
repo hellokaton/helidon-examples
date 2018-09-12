04084e3b78547107e235fafd81a933673be151dc
王爵 nice <biezhi.me@gmail.com>
2018-09-12 16:53:08 +0800

    :sparkles: add route error module

--- route-error/src/main/java/io/helidon/examples/error/Main.java
+++ route-error/src/main/java/io/helidon/examples/error/Main.java
@@ -1,8 +1,6 @@
 package io.helidon.examples.error;
 
-import io.helidon.webserver.Routing;
-import io.helidon.webserver.ServerConfiguration;
-import io.helidon.webserver.WebServer;
+import io.helidon.webserver.*;
 import io.helidon.webserver.json.JsonSupport;
 
 import javax.json.Json;
@@ -22,22 +20,8 @@ public class Main {
         Routing routing = Routing.builder()
                 .register(JsonSupport.get())
                 .get("/", (req, res) -> res.send("Hello World"))
-                .get("/exp", (req, res) -> {
-                    Optional<String> nameOptional = req.queryParams().first("name");
-                    try {
-                        String name = nameOptional.orElseThrow(() -> new MyException(2018, "name not is empty."));
-                        res.send("name is: " + name);
-                    } catch (MyException e) {
-                        req.next(e);
-                    }
-                })
-                .error(MyException.class, (req, res, ex) -> {
-                    JsonObject jsonObject = Json.createObjectBuilder().add("code", ex.getCode())
-                            .add("msg", ex.getMessage())
-                            .build();
-
-                    res.send(jsonObject);
-                })
+                .get("/exp", Main::exceptionHandle)
+                .error(MyException.class, (req, res, ex) -> myExceptionHandle(res, ex))
                 .error(Throwable.class, ((req, res, ex) ->
                         res.send(ex.getMessage())))
                 .build();
@@ -46,6 +30,24 @@ public class Main {
                 .port(2333)
                 .build(), routing);
         webServer.start();
+    }
+
+    private static void myExceptionHandle(ServerResponse res, MyException ex) {
+        JsonObject jsonObject = Json.createObjectBuilder().add("code", ex.getCode())
+                .add("msg", ex.getMessage())
+                .build();
+
+        res.send(jsonObject);
+    }
+
+    private static void exceptionHandle(ServerRequest req, ServerResponse res) {
+        Optional<String> nameOptional = req.queryParams().first("name");
+        try {
+            String name = nameOptional.orElseThrow(() -> new MyException(2018, "name not is empty."));
+            res.send("name is: " + name);
+        } catch (MyException e) {
+            req.next(e);
+        }
     }
 
 }
